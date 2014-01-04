20712796a48a4a5c30671c604b4ca7222677a86c
jochamo <jochamo@gmail.com>
2014-01-04 10:05:32 +0100

    login
    a

--- src/com/enterat/bda/Usuario.java
+++ src/com/enterat/bda/Usuario.java
@@ -40,5 +40,69 @@ public class Usuario {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
+	//Función para identificarse al abrir la aplicación
+	public static Usuario identificarse(String usuario, String clave)
+	{
+		String sql1 = "SELECT id_usuario, tipo FROM USUARIO ";
+		String sql2 = "WHERE usr LIKE '" + usuario + "' AND psw LIKE '" + clave + "'";
+		
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("sqlquery1", sql1));
+		pairs.add(new BasicNameValuePair("sqlquery2", sql2));
+				
+		JSONObject json;		
+		Usuario user = null;
+		
+		try {
+			//Obtener JSON
+			json = Conexion.obtenerJsonDelServicio(pairs, "service.executeSQL.php");
+			
+			//Si se ha obtenido...
+			if(json != null)
+			{
+				//
+				user = new Usuario();
+				//
+				user.setUser(usuario);
+				user.setPassword(clave);
+						
+				//
+				if (json.has("id_usuario"))
+				{							
+					user.setIdUsuario( Integer.parseInt(json.getString("id_usuario")) );						
+				}
+				
+				//
+				if (json.has("tipo"))
+				{							
+					user.setTipo( Integer.parseInt(json.getString("tipo")) );						
+				}				
+			}	
+				
+		} catch (ClientProtocolException c)	{
+			
+			//TODO tratar TODAS las excepciones !!!
+			
+			//throw new ExcepcionAplicacion(c.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			//throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			//throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (IOException e) {
+			e.printStackTrace();
+			//throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			//throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		}
+		
+		
+		//Devolver el usuario identificado
+		return user;
+	}
 	
 }
